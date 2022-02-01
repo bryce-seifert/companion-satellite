8dec35d8add906a35bda964fa1ab9f45ae2b09b8
Julian Waller <git@julusian.co.uk>
2022-02-01 19:06:49 +0000

    feat: shrink built image

---
+++ pi-image/shrink.sh
@@ -0,0 +1,272 @@
+#!/bin/bash
+
+# based on https://github.com/Drewsif/PiShrink/blob/43f1a885cd147638a0d1961753f711f838915b88/pishrink.sh
+
+version="v0.1.2"
+
+CURRENT_DIR="$(pwd)"
+SCRIPTNAME="${0##*/}"
+MYNAME="${SCRIPTNAME%.*}"
+LOGFILE="${CURRENT_DIR}/${SCRIPTNAME%.*}.log"
+REQUIRED_TOOLS="parted losetup tune2fs md5sum e2fsck resize2fs zerofree"
+
+function info() {
+	echo "$SCRIPTNAME: $1 ..."
+}
+
+function error() {
+	echo -n "$SCRIPTNAME: ERROR occurred in line $1: "
+	shift
+	echo "$@"
+}
+
+function cleanup() {
+	if losetup "$loopback" &>/dev/null; then
+		losetup -d "$loopback"
+	fi
+	if [ "$debug" = true ]; then
+		local old_owner=$(stat -c %u:%g "$src")
+		chown "$old_owner" "$LOGFILE"
+	fi
+
+}
+
+function logVariables() {
+	if [ "$debug" = true ]; then
+		echo "Line $1" >> "$LOGFILE"
+		shift
+		local v var
+		for var in "$@"; do
+			eval "v=\$$var"
+			echo "$var: $v" >> "$LOGFILE"
+		done
+	fi
+}
+
+function checkFilesystem() {
+	info "Checking filesystem"
+	e2fsck -pf "$loopback"
+	(( $? < 4 )) && return
+
+	info "Filesystem error detected!"
+
+	info "Trying to recover corrupted filesystem"
+	e2fsck -y "$loopback"
+	(( $? < 4 )) && return
+
+	error $LINENO "Filesystem recoveries failed. Giving up..."
+	exit 9
+
+}
+
+help() {
+	local help
+	read -r -d '' help << EOM
+Usage: $0 [-dpv] imagefile.img [newimagefile.img]
+
+  -v         Be verbose
+  -p         Remove logs, apt archives, dhcp leases and ssh hostkeys
+  -d         Write debug messages in a debug log file
+EOM
+	echo "$help"
+	exit 1
+}
+
+debug=false
+verbose=false
+prep=false
+
+while getopts ":adhprsvzZ" opt; do
+  case "${opt}" in
+    d) debug=true;;
+    h) help;;
+    p) prep=true;;
+    v) verbose=true;;
+    *) help;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ "$debug" = true ]; then
+	info "Creating log file $LOGFILE"
+	rm "$LOGFILE" &>/dev/null
+	exec 1> >(stdbuf -i0 -o0 -e0 tee -a "$LOGFILE" >&1)
+	exec 2> >(stdbuf -i0 -o0 -e0 tee -a "$LOGFILE" >&2)
+fi
+
+echo "${0##*/} $version"
+
+#Args
+src="$1"
+img="$1"
+
+#Usage checks
+if [[ -z "$img" ]]; then
+  help
+fi
+
+if [[ ! -f "$img" ]]; then
+  error $LINENO "$img is not a file..."
+  exit 2
+fi
+if (( EUID != 0 )); then
+  error $LINENO "You need to be running as root."
+  exit 3
+fi
+
+# set locale to POSIX(English) temporarily
+# these locale settings only affect the script and its sub processes
+
+export LANGUAGE=POSIX
+export LC_ALL=POSIX
+export LANG=POSIX
+
+#Check that what we need is installed
+for command in $REQUIRED_TOOLS; do
+  command -v $command >/dev/null 2>&1
+  if (( $? != 0 )); then
+    error $LINENO "$command is not installed."
+    exit 4
+  fi
+done
+
+#Copy to new file if requested
+if [ -n "$2" ]; then
+  f="$2"
+  info "Copying $1 to $f..."
+  cp --reflink=auto --sparse=always "$1" "$f"
+  if (( $? != 0 )); then
+    error $LINENO "Could not copy file..."
+    exit 5
+  fi
+  old_owner=$(stat -c %u:%g "$1")
+  chown "$old_owner" "$f"
+  img="$f"
+fi
+
+# cleanup at script exit
+trap cleanup EXIT
+
+#Gather info
+info "Gathering data"
+beforesize="$(ls -lh "$img" | cut -d ' ' -f 5)"
+parted_output="$(parted -ms "$img" unit B print)"
+rc=$?
+if (( $rc )); then
+	error $LINENO "parted failed with rc $rc"
+	info "Possibly invalid image. Run 'parted $img unit B print' manually to investigate"
+	exit 6
+fi
+partnum="$(echo "$parted_output" | tail -n 1 | cut -d ':' -f 1)"
+partstart="$(echo "$parted_output" | tail -n 1 | cut -d ':' -f 2 | tr -d 'B')"
+if [ -z "$(parted -s "$img" unit B print | grep "$partstart" | grep logical)" ]; then
+    parttype="primary"
+else
+    parttype="logical"
+fi
+loopback="$(losetup -f --show -o "$partstart" "$img")"
+tune2fs_output="$(tune2fs -l "$loopback")"
+rc=$?
+if (( $rc )); then
+    echo "$tune2fs_output"
+    error $LINENO "tune2fs failed. Unable to shrink this type of image"
+    exit 7
+fi
+
+currentsize="$(echo "$tune2fs_output" | grep '^Block count:' | tr -d ' ' | cut -d ':' -f 2)"
+blocksize="$(echo "$tune2fs_output" | grep '^Block size:' | tr -d ' ' | cut -d ':' -f 2)"
+
+logVariables $LINENO beforesize parted_output partnum partstart parttype tune2fs_output currentsize blocksize
+
+#Check if we should make pi expand rootfs on next boot
+if [[ $prep == true ]]; then
+  info "Syspreping: Removing logs, apt archives, dhcp leases and ssh hostkeys"
+  mountdir=$(mktemp -d)
+  mount "$loopback" "$mountdir"
+  rm -rvf $mountdir/var/cache/apt/archives/* $mountdir/var/lib/dhcpcd5/* $mountdir/var/log/* $mountdir/var/tmp/* $mountdir/tmp/* $mountdir/etc/ssh/*_host_*
+  umount "$mountdir"
+fi
+
+#Make sure filesystem is ok
+checkFilesystem
+
+echo "zerofree"
+zerofree "$loopback"
+
+if ! minsize=$(resize2fs -P "$loopback"); then
+	rc=$?
+	error $LINENO "resize2fs failed with rc $rc"
+	exit 10
+fi
+minsize=$(cut -d ':' -f 2 <<< "$minsize" | tr -d ' ')
+logVariables $LINENO currentsize minsize
+if [[ $currentsize -eq $minsize ]]; then
+  error $LINENO "Image already shrunk to smallest size"
+  exit 11
+fi
+
+#Add some free space to the end of the filesystem
+extra_space=$(($currentsize - $minsize))
+logVariables $LINENO extra_space
+for space in 5000 1000 100; do
+  if [[ $extra_space -gt $space ]]; then
+    minsize=$(($minsize + $space))
+    break
+  fi
+done
+logVariables $LINENO minsize
+
+#Shrink filesystem
+info "Shrinking filesystem"
+resize2fs -p "$loopback" $minsize
+rc=$?
+if (( $rc )); then
+  error $LINENO "resize2fs failed with rc $rc"
+  mount "$loopback" "$mountdir"
+  mv "$mountdir/etc/rc.local.bak" "$mountdir/etc/rc.local"
+  umount "$mountdir"
+  losetup -d "$loopback"
+  exit 12
+fi
+sleep 1
+
+#Shrink partition
+partnewsize=$(($minsize * $blocksize))
+newpartend=$(($partstart + $partnewsize))
+logVariables $LINENO partnewsize newpartend
+parted -s -a minimal "$img" rm "$partnum"
+rc=$?
+if (( $rc )); then
+	error $LINENO "parted failed with rc $rc"
+	exit 13
+fi
+
+parted -s "$img" unit B mkpart "$parttype" "$partstart" "$newpartend"
+rc=$?
+if (( $rc )); then
+	error $LINENO "parted failed with rc $rc"
+	exit 14
+fi
+
+#Truncate the file
+info "Shrinking image"
+endresult=$(parted -ms "$img" unit B print free)
+rc=$?
+if (( $rc )); then
+	error $LINENO "parted failed with rc $rc"
+	exit 15
+fi
+
+endresult=$(tail -1 <<< "$endresult" | cut -d ':' -f 2 | tr -d 'B')
+logVariables $LINENO endresult
+truncate -s "$endresult" "$img"
+rc=$?
+if (( $rc )); then
+	error $LINENO "trunate failed with rc $rc"
+	exit 16
+fi
+
+aftersize=$(ls -lh "$img" | cut -d ' ' -f 5)
+logVariables $LINENO aftersize
+
+info "Shrunk $img from $beforesize to $aftersize"
